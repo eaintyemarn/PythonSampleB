c474b64bbda999314ffd9fb3ccf90984d392baa8
“eaintyemarn” <eaintyemarn.510@gmail.com>
2019-09-08 12:01:35 +0630

    tax calculator

---
+++ cal.py
@@ -0,0 +1,11 @@
+width = 20
+height = 5 * 9
+vol = width * height
+print(vol)
+
+tax = 50 / 1000
+price= 3000
+totaltax = price * tax
+tprice = price + totaltax
+k=round(tprice)
+print(k)
---
+++ tax.py
@@ -0,0 +1,9 @@
+#Tax calculator
+
+tax = 50 / 1000
+price= 3000
+totaltax = price * tax
+tprice = price + totaltax
+k = round(tprice)
+print(k)
+
